cfeee6f7445b781a4e5d40ba742c6b0dac216ed4
Kevin Snow <krsnow77@zoho.com>
2014-06-25 16:43:05 -0600

    Edit of program
    Makes the program continue even if the input is not a number; asks user
    to try again

--- GuessThatNumber/GuessThatNumber/Program.cs
+++ GuessThatNumber/GuessThatNumber/Program.cs
@@ -27,60 +27,71 @@ namespace GuessThatNumber
             //declare function variables
             bool looping = true;
             Random randNumGen = new Random();
-            int guessCount = 1;
+            int guessCount = 0;
             //this grabs a single instance of the random number generator
             int randNum = randNumGen.Next(1, 101);
+            
+                
             //infinite loop of user input
             while (looping)
             {
-            
-            
-                 input = Console.ReadLine();
+                input = Console.ReadLine();
+
+                if ("qwertyuiopasdfghjklzxcvbnm~`!@#$%^&*()_-+=QWERTYUIOP{}|[]\\ASDFGHJKL:;\"\'ZXCVBNM<>?,./".Contains(input))
+                {
+                    Console.WriteLine("Sorry, not a number.Try again.");
+                    looping = false;
+                    GuessThatNumber("");
+                }
+
                 
-                int number = Convert.ToInt32(input);
-                guessCount++;
-                //condition to compare the input with the randNum
-                if (number == randNum)
-                {
-                    Console.WriteLine("Congrats you! You guessed it.");
-                    Console.WriteLine("It took you " + guessCount + " guesses to get it.");                   
-                    break;
-                }
-                else if (number > randNum && number - 10 <= randNum)
-                {
-                    Console.WriteLine("Ach so close but too high.");
-                }
-                else if (number < randNum && number + 10 >= randNum)
-                {
-                    Console.WriteLine("Hey...almost there but too low.");
-                }
-                else if (number < randNum)
-                {
-                    Console.WriteLine("Too low you. Maybe another guess.");
-                }
-                else if (number > randNum)
-                {
-                    Console.WriteLine("Too high. Way too high.");
-                }
-            }
-           
+                
+                    int number = Convert.ToInt32(input);
 
-            //ask for new game
-            Console.WriteLine("Would you like to play again?\nYes or No?");
+                    guessCount++;
+                    //condition to compare the input with the randNum
+                    if (number == randNum)
+                    {
+                        Console.WriteLine("Congrats you! You guessed it.");
+                        Console.WriteLine("It took you " + guessCount + " guesses to get it.");
+                        break;
+                    }
+                    else if (number > randNum && number - 10 <= randNum)
+                    {
+                        Console.WriteLine("Ach so close but too high.");
+                    }
+                    else if (number < randNum && number + 10 >= randNum)
+                    {
+                        Console.WriteLine("Hey...almost there but too low.");
+                    }
+                    else if (number < randNum)
+                    {
+                        Console.WriteLine("Too low you. Maybe another guess.");
+                    }
+                    else if (number > randNum)
+                    {
+                        Console.WriteLine("Too high. Way too high.");
+                    }
+                }
 
-            //create separate input for a new game
-            string input2 = Console.ReadLine();
 
-            //set up new game
-            if ("Yyesyaysiofcoursewhynotsuremaybe".Contains(input2))
-            {
-                Console.WriteLine("Put in another guess");
-                GuessThatNumber("");
-            }
-            else
-            {
+                //ask for new game
+                Console.WriteLine("Would you like to play again?\nYes or No?");
 
-            }
+                //create separate input for a new game
+                string input2 = Console.ReadLine();
+
+                //set up new game
+                if (input2 == "Yes" ||input2 == "yes" || input2 == "Y" || input2 == "y")
+                {
+                    Console.WriteLine("Put in another guess");
+                    GuessThatNumber("");
+                }
+                else
+                {
+
+                }
+            
         }
     }
 }
